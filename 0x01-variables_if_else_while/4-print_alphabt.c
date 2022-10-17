c0dbda9e698c64d2a65a5de2b0d42fc2f056ddc2
ajiboyetosin <ajiboyetosin18@yahoo.com>
2022-10-16 23:34:47 -0700

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints all alphabets except q and e in lower case
+ * main - prints all alphabets except q and e in lower case.
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
 {
