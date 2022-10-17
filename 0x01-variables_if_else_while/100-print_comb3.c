42089926b988c11f60356910c794628062ae4324
ajiboyetosin <ajiboyetosin18@yahoo.com>
2022-10-17 01:32:45 -0700

    bug fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,7 +14,7 @@ int main(void)
 {
 	int digit1, digit2;
 
-	for (digit1 = o; digit1 < 9; digit++)
+	for (digit1 = o; digit1 < 9; digit1++)
 	{
 		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
@@ -23,8 +23,8 @@ int main(void)
 
 			if (digit1 == 8 && digit2 == 9)
 				continue;
-			putchar(',')
-				putchar(' ')
+			putchar(',');
+				putchar(' ');
 		}
 	}
 	putchar('\n');
